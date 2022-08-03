06221d8196b0136a41fbaa0fdb058fd043419fb2
thejsman <niranjan.bhambi@hotmail.com>
2022-08-03 23:11:50 +0530

    update url

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ export const EventId = "MH5TyymECf8psfFrS";
 export const AppTItle = "EBIX HDFC MDRT";
 export const eventTitle = "EBIX HDFC MDRT";
 export const AppShortName = "HDFC MDRT";
-export const AppURL = "https://hdfcmart.thekeyevent.in";
+export const AppURL = "https://hdfc-mdrt.thekeyconference.in";
 export const isMobile = window.innerWidth <= 500;
